f4aaebac405d32fa5c53693076f096c0a31d8133
velor2012 <2281675608@qq.com>
2020-04-17 15:51:29 +0800

    添加点击特效

--- static/cnzz.js
+++
@@ -1,1 +0,0 @@
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1278822150'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1278822150' type='text/javascript'%3E%3C/script%3E"));
